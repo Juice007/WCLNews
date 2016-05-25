0e25be14c072784581a608926ba8a974d59de710
WCL-LV <lczzyc@qq.com>
2016-05-25 21:51:19 +0800

    Initial Commit

---
+++ WCLNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WCLNews
+//
+//  Created by Lv on 16/5/25.
+//  Copyright © 2016年 Lv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WCLNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WCLNews
+//
+//  Created by Lv on 16/5/25.
+//  Copyright © 2016年 Lv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
